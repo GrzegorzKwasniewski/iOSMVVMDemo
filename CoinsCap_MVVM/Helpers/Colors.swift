8d46ae6bc4ab4514946613507536b67d99927ac1
Grzesiek <grzegorz.kwasniewski@itcraft.pl>
2019-01-27 11:46:24 +0100

    ADD Custom Extensions And Helpers Objects

---
+++ CoinsCap_MVVM/Helpers/Colors.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+private enum BaseColors {
+    static let darkGray = #colorLiteral(red: 0.1764705882, green: 0.1764705882, blue: 0.1764705882, alpha: 1)
+    static let white = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+}
+
+
+enum Colors {
+    static let whiteBackground = BaseColors.white
+    static let whiteText = BaseColors.white
+    static let darkGreyText = BaseColors.darkGray    
+}
---
+++ CoinsCap_MVVM/Helpers/Fonts.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+
+enum Fonts {
+    
+    static var avenirBold: UIFont {
+        return try! getFont("Avenir-Bold")
+    }
+    
+    private static func getFont(_ fontName: String, with size: CGFloat = 16) throws -> UIFont {
+        guard let font = UIFont(name: fontName, size: size) else {
+            fatalError("""
+                Failed to load the \(fontName) font.
+                Make sure the font file is included in the project and the font name is spelled correctly.
+                """
+            )
+        }
+        return font
+    }
+}
