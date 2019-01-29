fb8d306e571570d5358431a589c842ac8bd15aa7
Grzesiek <grzegorz.kwasniewski@itcraft.pl>
2019-01-29 21:05:57 +0100

    ADD Start Application Flow From App Delegate

--- CoinsCap_MVVM/Resources/AppDelegate.swift
+++ CoinsCap_MVVM/Resources/AppDelegate.swift
@@ -9,11 +9,24 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    private var applicationCoordinator: ApplicationCoordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        startApplicationFlow()
+
         return true
+    }
+    
+    private func startApplicationFlow() {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let applicationCoordinator = ApplicationCoordinator(window: window)
+        
+        self.window = window
+        self.applicationCoordinator = applicationCoordinator
+        
+        applicationCoordinator.start()
     }
 }
 
